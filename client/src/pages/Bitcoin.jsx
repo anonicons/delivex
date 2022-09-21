0c0f0a3d198d1ba5cbb2a36b3bf2efe20d9bd48d
anonicons <93569544+Cybugg@users.noreply.github.com>
2022-09-21 14:28:31 +0100

    juicy

--- client/src/pages/Bitcoin.jsx
+++ client/src/pages/Bitcoin.jsx
@@ -7,12 +7,13 @@ function Bitcoin() {
       <div style={{minWidth:"300px"}} className="mx-auto shadow-max bg-white p-3 flex flex-col align-items-center container" >
         <h4 className='title-track fw-bold text-center text-black h3'>Payment Method:Bitcoin</h4>
         <img src="https://bitcoin.org/img/icons/logotop.svg?1662473327" alt="btc" width={300} height={200} />
-        <div className=''>
+        <div className='border rounded-2 p-2'>
          
         Total Amount: <span className='fw-bold'>$100</span>
-          <div className=''>Address:<span className='fw-bold'> bc1qceuwkzygqa3d9l4cn5x3e83cf2w8npk4qxnawk</span></div>
+          <div className='my-3'>Address:<span className='fw-bold'> bc1qceuwkzygqa3d9l4cn5x3e83cf2w8npk4qxnawk</span></div>
+          <div className='small-text'>Kindly copy the above address to make payments</div>
         </div>
-      <div className='small-text'>Kindly copy the above address to make payments</div>
+      
       <button className='btn btn-sm btn-warning mt-5 opacity-50'>Payment confirmed</button>
       <div className='small-text mb-5 pt-1'>Automatic confirmation system. <span className='text-decoration-underline cursor-pointer'>know more</span> </div>
       </div>

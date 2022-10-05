ad67b78e739ed638778e2c7f59be948c3fad8d4c
anonicons <93569544+Cybugg@users.noreply.github.com>
2022-10-05 17:30:00 +0100

    'hurray'

--- client/src/pages/ProductDes.jsx
+++ client/src/pages/ProductDes.jsx
@@ -22,7 +22,7 @@ useEffect(
  )
   return (
     <div className='product-details d-flex flex-column align-items-center justify-content-center fonta' >
-        <div className='d-flex align-items-center justify-content-center h4'>
+        <div className=' px-3 h5 text-secondary'>
         Package Estimated Arrival Time:<span className='fw-bold'> 4days</span> 
         </div>
         
